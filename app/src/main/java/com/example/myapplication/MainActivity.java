85f6f87a1999da70218676ecff82a6da1d44c895
SM <malla.s@northeastern.edu>
2023-11-07 20:50:11 -0800

    HomePageActivity and Buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Login
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homepageActivity();
+            }
+        }));
+
+    }
+
+    public void homepageActivity(){
+
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
+
     }
 }
