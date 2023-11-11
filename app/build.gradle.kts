c35999f9836554cc9c79582c8841cba0ce26903b
tickoo1993 <kartik.tickoo@gmail.com>
2023-11-10 19:10:54 -0500

    implemented login and registerion pages and their logic. creeated the entire flow user login flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+//    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myapplication"
@@ -27,6 +28,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -34,6 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -43,9 +49,9 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-database")
+//    implementation ("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-messaging:23.3.1")
-    implementation ("com.google.firebase:firebase-auth:22.2.0")
+//    implementation ("com.google.firebase:firebase-auth:22.2.0")
 
 }
--- app/src/main/java/com/example/myapplication/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/HomePageActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomePageActivity extends AppCompatActivity {
-
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String LOGGED_IN_KEY = "isLoggedIn";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button logoutButton = findViewById(R.id.buttonLogout);
+
+        logoutButton.setOnClickListener(v -> {
+
+            getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+                    .edit()
+                    .putBoolean(LOGGED_IN_KEY, false)
+                    .apply();
+
+            Intent intent = new Intent(HomePageActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
