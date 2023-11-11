bc94aec670cfe4cebe80a1e05d00f6611c1214fc
SM <malla.s@northeastern.edu>
2023-11-11 10:46:01 -0800

    Home Page buttons and activity

--- app/src/main/java/com/example/myapplication/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/HomePageActivity.java
@@ -2,13 +2,68 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    private Button budgetButton, expenseButton, dataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Budget
+        budgetButton = (Button) findViewById(R.id.budgetButton);
+        budgetButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                budgetActivity();
+            }
+        }));
+
+
+        // Expense
+        expenseButton = (Button) findViewById(R.id.expenseButton);
+        expenseButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                expenseActivity();
+            }
+        }));
+
+
+        // Data Visualization
+        dataButton = (Button) findViewById(R.id.dataButton);
+        dataButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataActivity();
+            }
+        }));
+
+
     }
+
+    public void budgetActivity(){
+
+        Intent intent = new Intent(this, budgetPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void expenseActivity(){
+
+        Intent intent = new Intent(this, expensePageActivity.class);
+        startActivity(intent);
+    }
+
+    public void dataActivity(){
+
+        Intent intent = new Intent(this, dataPageActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/budgetPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class budgetPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_page);
+    }
+}
