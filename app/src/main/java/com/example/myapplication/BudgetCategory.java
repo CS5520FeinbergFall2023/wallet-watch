a68c4e658b7221c0becf5661fe01ca3087d1d26d
dada-tech <david.olu.dada@gmail.com>
2023-11-12 16:25:06 -0700

    expense category initial value

--- app/src/main/java/com/example/myapplication/BudgetCategory.java
+++ app/src/main/java/com/example/myapplication/BudgetCategory.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+
 public class BudgetCategory {
     private String categoryName;
 
@@ -8,6 +10,12 @@ public class BudgetCategory {
     }
 
     public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
         return this.categoryName;
     }
 }
--- app/src/main/java/com/example/myapplication/ExpensePageActivity.java
+++ app/src/main/java/com/example/myapplication/ExpensePageActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO:
+ * 2. Date Popup
+ * 3. Get Values from inputs
+ * 4. Clear button
+ * 1. API Category Dropdown Options
+ * 5. API Submit
+ * 6. Clear values after submit
+ */
 public class ExpensePageActivity extends AppCompatActivity {
+
+    private List<BudgetCategory> categories;
+
+    private AutoCompleteTextView autoCompleteTextView;
+    private ArrayAdapter<BudgetCategory> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +35,13 @@ public class ExpensePageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_expense_page);
 
 
+        // Categories
+        categories = getInitialOptions();
+        autoCompleteTextView = findViewById(R.id.add_expense_category_text);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, categories);
+        autoCompleteTextView.setAdapter(adapter);
 
-
-
+        // Nav Bar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.expense);
 
@@ -53,5 +76,21 @@ public class ExpensePageActivity extends AppCompatActivity {
 
     public void showDatePickerDialog(View view) {
 
+    }
+
+    private void updateCategoryOptions(List<BudgetCategory> options) {
+        adapter.clear();
+        adapter.addAll(options);
+        adapter.notifyDataSetChanged();
+    }
+
+    // Will be an API call later
+    private List<BudgetCategory> getInitialOptions() {
+        List<BudgetCategory> options = new ArrayList<>();
+
+        options.add(new BudgetCategory("Food"));
+        options.add(new BudgetCategory("Travel"));
+        options.add(new BudgetCategory("Other"));
+        return options;
     }
 }
