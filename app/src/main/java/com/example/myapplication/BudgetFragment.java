73170fb996fe51b91ce11846fd034821ea151ec5
SM <malla.s@northeastern.edu>
2023-11-18 12:04:52 -0800

    BudgetPage

--- app/src/main/java/com/example/myapplication/BudgetFragment.java
+++ app/src/main/java/com/example/myapplication/BudgetFragment.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -7,11 +10,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import javax.annotation.Nullable;
+
 public class BudgetFragment extends Fragment {
 
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setTitle("Budget");
+        }
+
 
         return inflater.inflate(R.layout.fragment_budget_page, container, false);
     }
