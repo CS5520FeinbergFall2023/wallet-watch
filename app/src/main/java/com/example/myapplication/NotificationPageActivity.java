fac511a380367ca5a4c3a4a2ffb7347c18eb248b
kerry-ama <kaowusu03@gmail.com>
2023-11-18 12:26:17 -0500

    modified NotificationPageActivity.java and NotificationPageActivity.java

--- app/src/main/java/com/example/myapplication/NotificationPageActivity.java
+++ app/src/main/java/com/example/myapplication/NotificationPageActivity.java
@@ -1,19 +1,27 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class NotificationPageActivity extends AppCompatActivity {
     ArrayList<Notification> notificationList = new ArrayList<>();
     private User user;
+    private Dictionary<String, Integer> dictExpense = new Hashtable<>();
+    private Dictionary<String, Integer> budgetExpense = new Hashtable<>();
 
 
     @Override
@@ -25,14 +33,40 @@ public class NotificationPageActivity extends AppCompatActivity {
         headerTitle.setText(R.string.notification_history_string);
 
         FirebaseHelper helper = new FirebaseHelper();
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
+        DatabaseReference budgetDatabaseRef = FirebaseDatabase.getInstance().getReference()
                 .child("budgets")
                 .child(user.getUsername());
+        DatabaseReference expensesDatabaseRef = FirebaseDatabase.getInstance().getReference()
+                .child("expenses")
+                .child(user.getUsername());
+        budgetDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    for (DataSnapshot shot : snapshot.getChildren()) {
+                        if ("category".equalsIgnoreCase(shot.getKey())) {
+
+                        }
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
 
     }
 
     private void addNotification() {
+
 
     }
 
