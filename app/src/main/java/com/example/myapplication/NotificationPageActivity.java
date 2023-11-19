5a2535d3b63024f8dde2f2da6e97d9233b6647ff
kerry-ama <kaowusu03@gmail.com>
2023-11-19 11:24:50 -0500

    modified NotificationPageActivity.java

--- app/src/main/java/com/example/myapplication/NotificationPageActivity.java
+++ app/src/main/java/com/example/myapplication/NotificationPageActivity.java
@@ -20,10 +20,11 @@ import java.util.Hashtable;
 public class NotificationPageActivity extends AppCompatActivity {
     ArrayList<Notification> notificationList = new ArrayList<>();
     private User user;
+    //will hold category: aggregated expense amount
     private Dictionary<String, Integer> dictExpense = new Hashtable<>();
-    private Dictionary<String, Integer> budgetExpense = new Hashtable<>();
+    private Dictionary<String, Integer> dictBudget = new Hashtable<>();
 
-
+    //MOVE TO HOMESCREEN
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,12 +40,26 @@ public class NotificationPageActivity extends AppCompatActivity {
         DatabaseReference expensesDatabaseRef = FirebaseDatabase.getInstance().getReference()
                 .child("expenses")
                 .child(user.getUsername());
+
         budgetDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                //for (int i = 0; i < dataSnapshot.getChildren().size() {
+                    if (snapshot.getValue() == "Food" ) {
+
+
+                        //dictBudget.put("Food", dataSnapshot.getValue());
+                    }
+
                     for (DataSnapshot shot : snapshot.getChildren()) {
                         if ("category".equalsIgnoreCase(shot.getKey())) {
+
+
+
+                            //check budget/username/check if value is less than or equal to expenses/user/max value
+                            //if less than
+                            //homepage
 
                         }
                     }
